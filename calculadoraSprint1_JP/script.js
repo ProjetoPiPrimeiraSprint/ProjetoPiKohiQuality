90a68ab7112347963e752ae37ef78a4d0ae68e8c
Gustavo Gil <gustavo.gil@sptech.school>
2024-03-08 13:44:44 -0300

    Calculadora revisada

--- calculadoraSprint1_JP/script.js
+++ calculadoraSprint1_JP/script.js
@@ -6,13 +6,8 @@ var Porcentagem = Number(0.2); // redução de 20 a
 function calcular() {
     div1.innerHTML = ` `;
     var Produzidos = Number(inProduzido.value); // Sacas produzidas por ano
-<<<<<<< HEAD
     var KgProduzidos = Produzidos * 60; // Kg produzidos por ano
     var Perdido = Number(inPerda.value); // Kg perdidos por ano
-=======
-    var KgProduzido = Produzidos * 60; // Kg produzidos por ano
-    var Perdido = Number(inPerda.value); // Kg perdidos por ano++
->>>>>>> 42d3d98c213b48b102959980f2aca428f1a0aedc
     var option = Number(opcoes.value);
     var Total = KgProduzidos - Perdido;
 
@@ -21,14 +16,7 @@ function calcular() {
     TotalPerda = KgProduzidos - TotalPerda;
 
     if (option == 1) { // tipo de café = Arábica
-<<<<<<< HEAD
         Total *= ValorArabica; // valor com perdas sem nossos serviços
-=======
-        var Total = KgProduzido - Perdido;
-        Total *= ValorArabica; // valor sem nossos serviços
-        var TotalPerda = Perdido * Porcentagem - Produzidos; // total perdido
-        TotalPerda = (Produzidos - TotalPerda) * ValorArabica; // Aproveitamento
->>>>>>> 42d3d98c213b48b102959980f2aca428f1a0aedc
 
         TotalPerda *= ValorArabica; // Aproveitamento
         div1.innerHTML = `Ao não utilizar nossos processos de controle, você tem lucro final de café tipo Arábico de R$${Total.toFixed(4, 2)}<br><br>`;
