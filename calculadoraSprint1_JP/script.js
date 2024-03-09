565bf21dd548889404ba9c2c9d9b0560dc62dd95
Joao-Nocee <joao.noce@sptech.school>
2024-03-09 11:57:05 -0300

    Modificações em nomes e comentários

--- calculadoraSprint1_JP/script.js
+++ calculadoraSprint1_JP/script.js
@@ -1,44 +1,32 @@
-var ValorArabica = Number(1015 / 60).toFixed(4, 2); // valor de um Kg de Arábica
-var ValorConilon = Number(810 / 60).toFixed(4, 2); // valor de um Kg de Conilon
+var ValorArabica = Number(1015 / 60).toFixed(4, 2); // valor de 1 Kg de Arábica
+var ValorConilon = Number(810 / 60).toFixed(4, 2); // valor de 1 Kg de Conilon
 
-var Porcentagem = Number(0.2); // redução de 20 a 
+var Porcentagem = Number(0.2); // redução de perdas com nossos serviços
 
 function calcular() {
     div1.innerHTML = ` `;
     var Produzidos = Number(inProduzido.value); // Sacas produzidas por ano
-<<<<<<< HEAD
     var KgProduzidos = Produzidos * 60; // Kg produzidos por ano
     var Perdido = Number(inPerda.value); // Kg perdidos por ano
-=======
-    var KgProduzido = Produzidos * 60; // Kg produzidos por ano
-    var Perdido = Number(inPerda.value); // Kg perdidos por ano++
->>>>>>> 42d3d98c213b48b102959980f2aca428f1a0aedc
     var option = Number(opcoes.value);
-    var Total = KgProduzidos - Perdido;
+    var Total = KgProduzidos - Perdido; // Quantidade inicial de Kg de café sem nossos serviços
 
-    var TotalPerda = (KgProduzidos - Perdido) * Porcentagem; // total de perdas com a redução dos nossos serviços
+    var TotalPerda = (KgProduzidos - Perdido) * Porcentagem; // Total de perdas com a redução dos nossos serviços
 
-    TotalPerda = KgProduzidos - TotalPerda;
+    TotalPerda = KgProduzidos - TotalPerda; // Quantidade final de Kg de café com nossos serviços
 
-    if (option == 1) { // tipo de café = Arábica
-<<<<<<< HEAD
-        Total *= ValorArabica; // valor com perdas sem nossos serviços
-=======
-        var Total = KgProduzido - Perdido;
-        Total *= ValorArabica; // valor sem nossos serviços
-        var TotalPerda = Perdido * Porcentagem - Produzidos; // total perdido
-        TotalPerda = (Produzidos - TotalPerda) * ValorArabica; // Aproveitamento
->>>>>>> 42d3d98c213b48b102959980f2aca428f1a0aedc
+    if (option == 1) { // tipo de café = Arábico
+        Total *= ValorArabica; // Aproveitamento sem nossos serviços
 
-        TotalPerda *= ValorArabica; // Aproveitamento
+        TotalPerda *= ValorArabica; // Novo aproveitamento
         div1.innerHTML = `Ao não utilizar nossos processos de controle, você tem lucro final de café tipo Arábico de R$${Total.toFixed(4, 2)}<br><br>`;
 
         div1.innerHTML += `Seus lucros vão para R$${TotalPerda.toFixed(4, 2)} ao utilizar nossos serviços na Kohi Quality`;
     }
-    if (option == 2) {
-        Total *= ValorConilon; // valor com perdas sem nossos serviços
+    if (option == 2) { // tipo de café = Conilon
+        Total *= ValorConilon; // Aproveitamento sem nossos serviços
 
-        TotalPerda *= ValorConilon; // Aproveitamento
+        TotalPerda *= ValorConilon; // Novo aproveitamento
         div1.innerHTML = `Ao não utilizar nossos processos de controle, você tem lucro final de café tipo Conilon de R$${Total.toFixed(4, 2)}<br><br>`;
 
         div1.innerHTML += `Seus lucros vão para R$${TotalPerda.toFixed(4, 2)} ao utilizar nossos serviços na Kohi Quality`;
