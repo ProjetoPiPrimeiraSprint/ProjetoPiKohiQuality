34868248258c0f9b02f4ae16eb2503a248668609
yEllenCaroline <ellen.chaves@sptech.school>
2024-03-13 20:07:55 -0300

    Inserção de dados

--- Banco de Dados/Banco de Dados-Projeto Sprint1.sql
+++ Banco de Dados/Banco de Dados-Projeto Sprint1.sql
@@ -5,7 +5,7 @@ use sprint;
 create table Usuario(
 idUsuario int primary key auto_increment,
 nome varchar(40),
-endereço varchar(40),
+endereco varchar(40),
 CPF char(11) unique,
 email varchar(40) unique,
 senha char(8),
@@ -23,14 +23,6 @@ nome varchar(40),
 valor varchar(40),
 data_hora datetime default current_timestamp);
 
-
-create table Regiao(
-idRegiao int primary key auto_increment,
-localizacao varchar(40),
-data_hora datetime,
-manutencao datetime,
-idUsuario int);
-
 create table Armazem(
 idArmazem int primary key auto_increment,
 nome varchar (50),
@@ -43,3 +35,25 @@ idGrão int primary key auto_increment,
 nome varchar(30),	
 especificacoes varchar(60),
 umidade float);
+
+
+insert into Usuario(nome,endereco,CPF,email,senha,contato) values
+('José Santanna','Rua Inglaterra, 91','09534253319','josesantanna@gmail.com','1535','(18)3265-1329'),
+('São João da Silva','R. Com. Bichara Moherdauí, 191','09423442343','saojoao@gmail.com','5698','(11)99935-0594'),
+('Geronimo Alves','R. Celestino Gonçalves Bueno, 120','2344689898','geronimo@gmail.com','4002','(11)99918-3851'),
+('Fabiana Santos','R. Nove de Julho, 453','43523478822','fabianasantos@gmail.com','8922','(21)3432-5541'),
+('Clotilde Ferreira','R. Doce Caminho do Mar','34265888523','clotildeferreira@gmail.com','1212','(13)4454-3642');
+
+insert into SensorUmidade(nome,valor)values
+('EE160','35.8'),
+('EE072','27.53'),
+('EE212','26.68'),
+('EE260','25.20'),
+('EE23','27.36');
+
+insert into SensorTemperatura(nome,valor)values
+('EE074','19.5'),
+('EE431','17.6'),
+('EE441','18.9'),
+('EE471','14.8'),
+('EE889','16.4');
