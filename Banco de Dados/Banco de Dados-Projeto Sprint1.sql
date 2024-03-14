0cd81a2643eb4e59aa42d274b2557feeed2adf30
yEllenCaroline <ellen.chaves@sptech.school>
2024-03-13 21:10:14 -0300

    Inserção de dados concluido!

--- Banco de Dados/Banco de Dados-Projeto Sprint1.sql
+++ Banco de Dados/Banco de Dados-Projeto Sprint1.sql
@@ -13,28 +13,28 @@ contato varchar(14) unique);
 
 create table SensorUmidade(
 idSensor int primary key auto_increment,
-nome varchar(40),
-valor varchar(40),
+nome varchar(15),
+valor varchar(10),
 data_hora datetime default current_timestamp);
 
 create table SensorTemperatura(
 idSensor int primary key auto_increment,
-nome varchar(40),
-valor varchar(40),
+nome varchar(15),
+valor varchar(10),
 data_hora datetime default current_timestamp);
 
 create table Armazem(
 idArmazem int primary key auto_increment,
-nome varchar (50),
-capacidade float,
-localização varchar(50),
+modelo varchar (20),
+capacidadeTonelada float,
+localizacao varchar(50),
 responsavel varchar(50));
 
 create table ProdutoeGraos(
 idGrão int primary key auto_increment,
-nome varchar(30),	
-especificacoes varchar(60),
-umidade float);
+nome varchar(20),	
+temperatura varchar(20),
+umidade varchar(20));
 
 
 insert into Usuario(nome,endereco,CPF,email,senha,contato) values
@@ -42,7 +42,7 @@ insert into Usuario(nome,endereco,CPF,email,senha,contato) values
 ('São João da Silva','R. Com. Bichara Moherdauí, 191','09423442343','saojoao@gmail.com','5698','(11)99935-0594'),
 ('Geronimo Alves','R. Celestino Gonçalves Bueno, 120','2344689898','geronimo@gmail.com','4002','(11)99918-3851'),
 ('Fabiana Santos','R. Nove de Julho, 453','43523478822','fabianasantos@gmail.com','8922','(21)3432-5541'),
-('Clotilde Ferreira','R. Doce Caminho do Mar','34265888523','clotildeferreira@gmail.com','1212','(13)4454-3642');
+('Clotilde Ferreira','R. Doce Caminho do Mar 563','34265888523','clotildeferreira@gmail.com','1212','(13)4454-3642');
 
 insert into SensorUmidade(nome,valor)values
 ('EE160','35.8'),
@@ -57,3 +57,28 @@ insert into SensorTemperatura(nome,valor)values
 ('EE441','18.9'),
 ('EE471','14.8'),
 ('EE889','16.4');
+
+insert into Armazem(modelo,capacidadeTonelada,localizacao,responsavel) values
+('SAT 0405','64.20','Rua Inglaterra, 91','José Santana'),
+('SAT 0508','145.80','R. Com. Bichara Moherdauí, 191','São joão da Silva'),
+('SAT 0705','174.70','Rua Celestinho Gonçalves Bueno, 120','Geronimo Alves'),
+('SAT 0406','74.20','Rua Nove de Julho, 453','Fabiana Santos'),
+('SAT 0811','462','Rua Doce Caminho do Mar, 563','Clotilde Ferreira');
+
+insert into ProdutoeGraos(nome,temperatura,umidade) values
+('Connilon','22°C ~ 26°C','16 ~ 18%'),
+('Arábica','19°C ~ 21°C','11 ~ 13%');
+
+
+select * from Usuario;
+select * from Usuario where idUsuario=3;
+
+select * from SensorUmidade;
+select * from SensorUmidade where nome = 'EE23';
+
+select * from SensorTemperatura;
+select * from SensorTemperatura where valor like '%19%';
+
+select * from Armazem;
+select * from Armazem where responsavel like '%José%';
+
